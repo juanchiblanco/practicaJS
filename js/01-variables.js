53450587dea9affe80141b31bcacde9aa650e452
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-07 20:40:30 -0300

    agregamos opciones de como mostrar mensajes

---
+++ js/01-variables.js
@@ -0,0 +1,12 @@
+//Esto es un comentario de una linea - ES6 - 2015
+
+/*Esto es un comentario de multiples lineas*/
+
+console.log("hola mundo");
+
+document.writeln("<p>Hola Mundo</p>")
+document.writeln("Hola Mundo")
+
+//Ventana emergente
+
+//alert("Hola Alert")
