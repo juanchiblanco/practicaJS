c9206e318a1e9e1ec235f24e1d79bbab4ba6ccfd
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-07 21:10:58 -0300

    agregamos un ejemplo de variables con let

--- js/01-variables.js
+++ js/01-variables.js
@@ -10,3 +10,12 @@ document.writeln("Hola Mundo")
 //Ventana emergente
 
 //alert("Hola Alert")
+
+//como declarar una variable (let - const - var)
+
+let actualYear
+//inicializar una variable
+actualYear = 2025
+
+document.writeln("<br>El año actual es: ", actualYear)
+
