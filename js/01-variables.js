6f2c0833a53b1266aa9cb98520b210c265e3667b
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-09 20:31:11 -0300

    agregamos un ejemplo de todos los tipos de datos

--- js/01-variables.js
+++ js/01-variables.js
@@ -34,3 +34,72 @@ console.log(numero2)
 
 document.writeln("<br> El resultado de la suma es: ", resultado = numero1 + numero2)
 
+//Tipos de datos primitivos
+//string (texto)
+const nombreUsuario = "Juan Manuel Blanco"
+const producto = 'Celular'
+const tarea = `Realizar el tp 1`
+//number
+const edad = 30;
+const precio = 35.6;
+const precioNegativo = -20.5;
+//booleanos
+
+const encendido = true
+const lavado = false
+
+//null: es un valor vacio
+let vacio = null;
+
+//undefined
+
+let datoNuevo;
+
+console.log(nombreUsuario)
+console.log(producto)
+console.log(tarea)
+console.log(edad)
+console.log(precio)
+console.log(precioNegativo)
+console.log(encendido)
+console.log(lavado)
+console.log(vacio)
+console.log(datoNuevo)
+
+//Tipos de datos referenciales
+
+//objeto con notacion literal
+const funko = {
+    nombre: "Ironman",
+    precio: 30,
+    codigo: "ID540"
+}
+
+console.log(nombreUsuario)
+console.log(producto)
+console.log(tarea)
+console.log(edad)
+console.log(precio)
+console.log(precioNegativo)
+console.log(encendido)
+console.log(lavado)
+console.log(vacio)
+console.log(datoNuevo)
+console.log(funko)
+
+//arrays
+
+const productos = ["celular", "tablet", "notebook"]
+
+console.log(nombreUsuario)
+console.log(producto)
+console.log(tarea)
+console.log(edad)
+console.log(precio)
+console.log(precioNegativo)
+console.log(encendido)
+console.log(lavado)
+console.log(vacio)
+console.log(datoNuevo)
+console.log(funko)
+console.log(productos)
