ec38313e6eca3f6e1e445db698992501a2847765
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-19 19:55:04 -0300

    agregamos ejemplo de estructura de un ejercio con switch

---
+++ js/03-switch.js
@@ -0,0 +1,9 @@
+// opcion = 'sabado'
+// switch(opcion){
+// case 'lunes': todas las lineas y despues break;
+// case 'martes': todas las lineas y despues break;
+//  }
+
+//Ejercicio cajero automatico
+
+//Pedir al usuario que ingrse una opcion 1-consultar saldo, 2-depositar dinero. 3-extraer dinero, 4-salir
