1088967b695bc74aaf636b14d5ab7465f9dfee8e
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-21 20:38:44 -0300

    dibujamos la cantidad de filas que pide el usuario

---
+++ js/ejercicios/tp2-ejercicio10.js
@@ -0,0 +1,15 @@
+// 10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
+
+const filas = parseInt(prompt(`Ingresa un numero de filas`))
+const columnas = parseInt(prompt(`Ingresa un numero de columnas`))
+
+document.writeln(`<table>
+           <tbody>`)
+for(let indiceFilas = 1; indiceFilas<=filas; indiceFilas++){
+    document.writeln(`
+                <tr>
+                    <td>i</td>
+                </tr>`)}
+document.writeln(`
+           </tbody>
+        </table>`)
