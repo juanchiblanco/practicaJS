d2718c1a128f096e9fbe2c4eb6a57f974c68208e
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-21 19:51:32 -0300

    logramos mostrar los 30 renglones de la piramide

---
+++ js/ejercicios/tp2-ejercicio6.js
@@ -0,0 +1,10 @@
+// const numero = 1
+
+// while (numero<= 30){document.writeln(`<p>${numero}</p>`)
+//     numero++
+// }
+
+for(let i=1; i<=30;i++)
+    {document.writeln(i)
+        document.writeln(`<Br>`)   
+    }
