8d65e501662917bfb455585510a8d67ea55de506
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-21 09:17:21 -0300

    agregamos un ejemplo de while

---
+++ js/04-bucles.js
@@ -0,0 +1,13 @@
+//estructuras de repetición
+//while - do-while - for
+
+//While: mientras se cumple una condicion logica, hace tal cosa - while(condicion logica)
+// {
+// hace esto
+//agregar una linea donde la condicion logica se deje de cumplir} 
+
+let renglon = 1
+
+while (renglon<= 50){document.writeln(`<p>Renglon numero ${renglon}</p>`)
+    renglon = renglon + 1
+}
