7bc0f33e2dcb867265628ae073cd944b9e6c7b3e
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-28 20:10:21 -0300

    mejoramos el codigo con un funcion que reemplaza lo que repetimos muchas veces

--- js/05-arrays.js
+++ js/05-arrays.js
@@ -4,7 +4,16 @@ const paises = []
 
 //Declarar un array con datos
 
-const comidas = [`🍔`,2,true,`pizza`]
+const comidas = [`🍔`,2,true,`pizza`];
+
+function listarComidas(titulo){
+    document.writeln(`<h3>${titulo}</h3>`)
+
+    document.writeln(`<ul>`)
+for(let i=0; i<comidas.length; i++){
+    document.writeln(`<li>${comidas[i]}</li>`)}
+document.writeln(`</ul>`)
+}
 
 //mostrar el array conosola
 
@@ -17,66 +26,80 @@ document.writeln(paises+`<br>`)
 document.writeln(comidas)
 document.writeln(`<br>Cantidad de elementos en el array: ${comidas.length}`)
 
-document.writeln(`<ul>`)
-for(let i=0; i<comidas.length; i++){
-    document.writeln(`<li>${comidas[i]}</li>`)}
-document.writeln(`</ul>`)
+// document.writeln(`<ul>`)
+// for(let i=0; i<comidas.length; i++){
+//     document.writeln(`<li>${comidas[i]}</li>`)}
+// document.writeln(`</ul>`)
+
+listarComidas(`Mostrar el array de comidas`)
 
 //Agregar elementos al array
 
-document.writeln("<h3>Agregar un elemento al principio del array</h3>")
+// document.writeln("<h3>Agregar un elemento al principio del array</h3>")
 
 comidas.unshift(`🍟`,`🌭`)
 
-document.writeln(`<ul>`)
-for(let i=0; i<comidas.length; i++){
-    document.writeln(`<li>${comidas[i]}</li>`)}
-document.writeln(`</ul>`)
+listarComidas(`Agregar un elemento al principio del array`)
 
-document.writeln("<h3>Agregar un elemento al final del array</h3>")
+// document.writeln(`<ul>`)
+// for(let i=0; i<comidas.length; i++){
+//     document.writeln(`<li>${comidas[i]}</li>`)}
+// document.writeln(`</ul>`)
+
+// document.writeln("<h3>Agregar un elemento al final del array</h3>")
 
 comidas.push(`🧀`,`🥖`)
 
-document.writeln(`<ul>`)
-for(let i=0; i<comidas.length; i++){
-    document.writeln(`<li>${comidas[i]}</li>`)}
-document.writeln(`</ul>`)
+listarComidas(`Agregar un elemento al final del array`)
 
-document.writeln("<h3>Agregar un elemento en el medio del array</h3>")
+// document.writeln(`<ul>`)
+// for(let i=0; i<comidas.length; i++){
+//     document.writeln(`<li>${comidas[i]}</li>`)}
+// document.writeln(`</ul>`)
+
+// document.writeln("<h3>Agregar un elemento en el medio del array</h3>")
 
 comidas.splice(3,0,`🍝`)
 
-document.writeln(`<ul>`)
-for(let i=0; i<comidas.length; i++){
-    document.writeln(`<li>${comidas[i]}</li>`)}
-document.writeln(`</ul>`)
+listarComidas(`Agregar un elemento en el medio del array`)
 
-document.writeln("<h3>Borrar un elemento al principio del array</h3>")
+// document.writeln(`<ul>`)
+// for(let i=0; i<comidas.length; i++){
+//     document.writeln(`<li>${comidas[i]}</li>`)}
+// document.writeln(`</ul>`)
+
+// document.writeln("<h3>Borrar un elemento al principio del array</h3>")
 
 comidas.shift()
 
-document.writeln(`<ul>`)
-for(let i=0; i<comidas.length; i++){
-    document.writeln(`<li>${comidas[i]}</li>`)}
-document.writeln(`</ul>`)
+listarComidas(`Borrar un elemento al principio del array`)
 
-document.writeln("<h3>Borrar el ultimo elemento del array</h3>")
+// document.writeln(`<ul>`)
+// for(let i=0; i<comidas.length; i++){
+//     document.writeln(`<li>${comidas[i]}</li>`)}
+// document.writeln(`</ul>`)
+
+// document.writeln("<h3>Borrar el ultimo elemento del array</h3>")
 
 comidas.pop()
 
-document.writeln(`<ul>`)
-for(let i=0; i<comidas.length; i++){
-    document.writeln(`<li>${comidas[i]}</li>`)}
-document.writeln(`</ul>`)
+listarComidas(`Borrar el ultimo elemento del array`)
 
-document.writeln("<h3>Borrar un elemento en el medio del array</h3>")
+// document.writeln(`<ul>`)
+// for(let i=0; i<comidas.length; i++){
+//     document.writeln(`<li>${comidas[i]}</li>`)}
+// document.writeln(`</ul>`)
+
+// document.writeln("<h3>Borrar un elemento en el medio del array</h3>")
 
 comidas.splice(3,1)
 
-document.writeln(`<ul>`)
-for(let i=0; i<comidas.length; i++){
-    document.writeln(`<li>${comidas[i]}</li>`)}
-document.writeln(`</ul>`)
+listarComidas(`Borrar un elemento en el medio del array`)
+
+// document.writeln(`<ul>`)
+// for(let i=0; i<comidas.length; i++){
+//     document.writeln(`<li>${comidas[i]}</li>`)}
+// document.writeln(`</ul>`)
 
 /*document.writeln("<h3>Borrar mas de un elemento del array</h3>")
 
@@ -96,11 +119,13 @@ for(let i=0; i<comidas.length; i++){
     document.writeln(`<li>${comidas[i]}</li>`)}
 document.writeln(`</ul>`)*/
 
-document.writeln("<h3>Modificar un elemento del array</h3>")
+// document.writeln("<h3>Modificar un elemento del array</h3>")
 
 comidas[4] = `🍪`
 
-document.writeln(`<ul>`)
-for(let i=0; i<comidas.length; i++){
-    document.writeln(`<li>${comidas[i]}</li>`)}
-document.writeln(`</ul>`)
+listarComidas(`Modificar un elemento del array`)
+
+// document.writeln(`<ul>`)
+// for(let i=0; i<comidas.length; i++){
+//     document.writeln(`<li>${comidas[i]}</li>`)}
+// document.writeln(`</ul>`)
